6fbaac3cb32e11633e303a9e421098d6132f7314
MICHELLE-TOWER\Michelle <kogelmichelle@gmail.com>
2017-04-03 22:24:31 +0200

    Added initial template and style files. They are currently in a usable state, but are not strictly a blank template. Custom styling especially focuses on dropshadow and window boundary customisation.

---
+++ Mm.Wpf.Controls/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+
+namespace Mm.Wpf.Controls
+{
+    public class CustomWindow : Window
+    {
+        #region Click events
+        protected void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        protected void RestoreClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        protected void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        static CustomWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomWindow),
+                new FrameworkPropertyMetadata(typeof(CustomWindow)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            Button minimizeButton = GetTemplateChild("minimizeButton") as Button;
+            if (minimizeButton != null)
+                minimizeButton.Click += MinimizeClick;
+
+            Button closeButton = GetTemplateChild("closeButton") as Button;
+            if (closeButton != null)
+                closeButton.Click += CloseClick;
+
+            Rectangle moveRect = GetTemplateChild("moveRectangle") as Rectangle;
+            if (moveRect != null)
+                moveRect.PreviewMouseDown += moveRectangle_PreviewMouseDown;
+
+            Label WindowTitle = GetTemplateChild("WindowTitle") as Label;
+            WindowTitle.Content = Title;
+
+            Image WindowIcon = GetTemplateChild("WindowIcon") as Image;
+            WindowIcon.Source = Icon;
+
+            base.OnApplyTemplate();
+        }
+
+        private void moveRectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
