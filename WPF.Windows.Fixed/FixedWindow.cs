0b543890e984a1b17328739a8e531fedb2f2c5bf
Michelle Anne Funcke <michelle.funcke@derivco.co.za>
2017-07-26 15:35:19 +0200

    Added the FixedWindow class. Haven't editted style yet.

---
+++ WPF.Windows.Fixed/FixedWindow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Media.Effects;
+using System.Windows.Media;
+
+namespace WPF.Windows.Fixed
+{
+    public class FixedWindow : Window
+    {
+        #region Click events
+        protected void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+    }
+}
